94ca1dc830b6f0f24876b0be1646532f49922276
ja_mescher <james.hoelscher09@gmail.com>
2018-09-02 13:32:05 -0400

    Observation ui elements

---
+++ src/components/ObservationColorSelect.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+
+import { ColorOptionsList, ColorDescriptions } from '../constants'
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: theme.spacing.unit / 4,
+  },
+});
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+class MultipleSelect extends React.Component {
+  state = {
+    option: [],
+  };
+
+  handleChange = event => {
+    this.setState({ option: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="select-multiple-chip">Chip</InputLabel>
+          <Select
+            multiple
+            value={this.state.option}
+            onChange={this.handleChange}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={selected => (
+              <div className={classes.chips}>
+                {selected.map(value => (
+                  <Chip key={value} label={value} className={classes.chip} />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {ColorOptionsList.map(option => {
+              const label = ColorDescriptions[option]['shortDesc'] + ' - ' + ColorDescriptions[option]['longDesc']
+              return (
+                <MenuItem key={option} value={label}>
+                  <Checkbox checked={this.state.option.indexOf(label) > -1} />
+                  <ListItemText
+                    primary={
+                      label
+                    }
+                  />
+                </MenuItem>
+              )
+            })}
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
+}
+
+MultipleSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(MultipleSelect);
